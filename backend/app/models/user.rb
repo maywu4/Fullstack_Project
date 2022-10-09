831f3179e97e527ef9584b051976d9e25ab547ee
May Wu <maywu4@gmail.com>
2022-10-09 16:23:52 -0400

    setup backend user auth part 2

---
+++ backend/app/models/user.rb
@@ -0,0 +1,72 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string           not null
+#  email           :string           not null
+#  first_name      :string           not null
+#  last_name       :string           not null
+#  age             :integer          not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+class User < ApplicationRecord
+    has_secure_password
+
+    validates :first_name, :last_name, presence: true
+
+    validates :age, presence: true, numericality: { greater_than_or_equal_to: 13 }
+
+    validates :session_token, presence: true, uniqueness: true
+
+    validates :username, uniqueness: true
+
+    validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP } 
+
+    validates :password, length: { minimum: 12 }, allow_nil: true
+
+    before_validation :ensure_session_token
+
+    def self.find_by_credentials(credential, password)
+
+        if credential.match(URI::MailTo::EMAIL_REGEXP)
+            user = User.find_by(email: credential)
+        else 
+            user = User.find_by(username: credential)
+        end
+
+        if user && user.authenticate(password)
+            user
+        else
+            nil
+        end
+
+    end
+
+    def reset_session_token!
+        self.update!(session_token: generate_unique_session_token)
+        self.session_token
+    end
+
+
+    private 
+
+    def generate_unique_session_token
+        token = SecureRandom.base64
+
+        while User.exists?(session_token: token)
+            token = SecureRandom.base64
+        end
+
+        token
+    end
+
+    def ensure_session_token
+        self.session_token ||= generate_unique_session_token
+    end
+
+
+end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -4,6 +4,13 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
+  #route only active in development to test user auth
+  post 'api/test', to: 'application#test'
+
+  namespace :api, defaults: { format: :json } do
+    resources :users, only: :create
+    resource :session, only: [:show, :create, :destroy]
+  end
 
   #catch-all route after all of other routes
   get '*path', to: "static_pages#frontend_index"
