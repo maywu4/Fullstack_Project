22e7333e90c5009c2ebbf7d394496f36ce46232f
May Wu <maywu4@gmail.com>
2022-11-15 20:09:11 -0500

    Update favicon and photostream on profile

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -1,7 +1,7 @@
 import { useSelector, useDispatch } from "react-redux";
 import { Redirect } from "react-router-dom";
 import { getPosts, fetchPosts } from "../../store/posts";
-import { useEffect} from "react";
+import { useEffect, useState} from "react";
 import Navigation from "../Navigation";
 import PostItem from "./postItem";
 import NewPostForm from "../NewPostForm";
@@ -11,18 +11,20 @@ const HomePage = () => {
     const dispatch = useDispatch();
     const currentUser = useSelector(state => state.session.user);
     const posts = useSelector(getPosts);
+    const [reload, setReload] = useState(0);
 
 
     useEffect(() => {
         dispatch(fetchPosts());
-    },[]);
+    },[dispatch, reload]);
     
    
     if (!currentUser) return <Redirect exact to="/" />;
     if (!posts) return null;
 
     const postItems = posts.reverse().map((post) => (<PostItem key={post.id} post={post} />))
-    
+
+    console.log(posts.reverse())
     return (
         <div className="homePage">
             <Navigation/>
@@ -33,7 +35,7 @@ const HomePage = () => {
                     {/* {posts ? posts.map((post) => (<ul><li>{post}</li></ul>)) : null } */}
                     { postItems }
                 </ul>
-                <NewPostForm/>
+                <NewPostForm setReload={setReload} reload={reload}/>
             </div>
         </div>
     )
