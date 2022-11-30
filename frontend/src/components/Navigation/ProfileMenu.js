0487fe1a1e65dac255412e37c6df296897603135
May Wu <maywu4@gmail.com>
2022-11-29 21:54:23 -0500

    Update profile menu to link to user profile and settings

--- frontend/src/components/Navigation/ProfileMenu.js
+++ frontend/src/components/Navigation/ProfileMenu.js
@@ -5,6 +5,7 @@ const ProfileMenu = ({setSelectTab}) => {
     const [showMenu, setShowMenu] = useState(false);
     const currentUser = useSelector(state => state.session.user);
     let profilePath = `/people/${currentUser.id}`
+    let settingsPath = `/${currentUser.id}/account-settings`
 
     
     const openMenu = () => {
@@ -14,11 +15,13 @@ const ProfileMenu = ({setSelectTab}) => {
 
     const linksList = (
         <ul className="profile-dropdown" id="profileLinksList">
-            <li><a href={profilePath}><button onClick={ () => setSelectTab('aboutTab') }>About</button></a></li>
+            {/* <li><a href={profilePath}><button onClick={ () => setSelectTab('aboutTab') }>About</button></a></li> */}
+            <li><a href={profilePath}><button >Profile</button></a></li>
             {/* <br /> */}
-            <li><a href={profilePath}><button onClick={() => setSelectTab('photostreamTab')}>Photostream</button></a></li>
+            {/* <li><a href={profilePath}><button onClick={() => setSelectTab('photostreamTab')}>Photostream</button></a></li> */}
+            <li><a href={settingsPath}><button >Settings</button></a></li>
             {/* <br /> */}
-            <li><a href={profilePath}><button onClick={() => setSelectTab('favesTab')}>Faves</button></a></li>
+            {/* <li><a href={profilePath}><button onClick={() => setSelectTab('favesTab')}>Faves</button></a></li> */}
         </ul>
     );
 
