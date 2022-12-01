9a08baf64484d8e9ba69882859adf51a5624f70a
May Wu <maywu4@gmail.com>
2022-12-01 15:20:16 -0500

    Correct misspellings in routes file and likes controller

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
     resources :users, only: [:create, :show, :update]
     resources :posts, only: [:index, :create, :show, :update, :destroy]
     resources :comments, only: [:index, :create, :show, :update, :destroy]
-    resources :likes, only: [:index, :create, :show :destroy]
+    resources :likes, only: [:index, :create, :show, :destroy]
     resource :session, only: [:show, :create, :destroy]
   end
 
