2b7ad9ff808772e4f30ba5860de489ac118060ee
May Wu <maywu4@gmail.com>
2022-12-07 19:41:50 -0500

    Add like and comments icons to post items in homepage

--- frontend/src/components/HomePage/postItem.js
+++ frontend/src/components/HomePage/postItem.js
@@ -2,6 +2,13 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchUser, getUser } from "../../store/user";
 import { NavLink} from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
+import { faStar as emptyStar } from '@fortawesome/free-regular-svg-icons'
+import { faComment } from '@fortawesome/free-solid-svg-icons'
+import { faComment as emptyComment } from '@fortawesome/free-regular-svg-icons'
+import { fetchLikes, getLikes } from "../../store/likes";
+import { fetchComments, getComments } from "../../store/comments";
 
 
 const PostItem = ({post}) => {
@@ -10,16 +17,31 @@ const PostItem = ({post}) => {
     const posterProfileLink = post ? `/people/${post.posterId}` : null
     const postShowLink = `/photos/${post.id}`
     const postPoster = useSelector(getUser(post.posterId));
+    const likes = useSelector(getLikes);
+    const comments = useSelector(getComments);
 
     useEffect(() => {
-        dispatch(fetchUser(post.posterId))
+        dispatch(fetchUser(post.posterId));
+        dispatch(fetchLikes());
+        dispatch(fetchComments());
     }, [])
+
+
+    const numLikes = likes.filter((like) => {
+        if (like.postId === parseInt(post.id, 10)) return true;
+        return false;
+    }).length
+
+    const numComments = comments.filter((comment) => {
+        if (comment.postId === post.id) return true;
+        return false;
+    }).length
 
     return (
         <div className="postItem">
             <div className='postPosterInfo'>
                 {/* {posterImg} */}
-                <NavLink to={posterProfileLink}><img className='commentorProfilePic' src={postPoster ? postPoster.picture : null} alt="user-profile-pic" /></NavLink>
+                <NavLink to={posterProfileLink}><img className='posterProfilePic' src={postPoster ? postPoster.picture : null} alt="user-profile-pic" /></NavLink>
                 {/* {post.poster.picture === null ? <h6>Null</h6> : <h6>Not Null</h6> } */}
                 <div >
                     <NavLink to={posterProfileLink}>
@@ -33,10 +55,28 @@ const PostItem = ({post}) => {
                 <NavLink to={`/photos/${post.id}`}><img src={post.picture} alt='' /></NavLink>
             </div>
             <div className="postDetails">
-                <NavLink to={`/photos/${post.id}`}><h6 >{post.title ? post.title : null}</h6></NavLink>
-                
-                <div >{post.description ? post.description : null }</div>
+                <div id="postDeets">
+                    <NavLink to={`/photos/${post.id}`}><h6 >{post.title ? post.title : null}</h6></NavLink>
+                    
+                    <div >{post.description ? post.description : null }</div>
+                </div>
+                <div id="postNums">
+                    <div id="postLikesSummary">
+                        {numLikes !== 0 ? <FontAwesomeIcon icon={faStar} /> : <FontAwesomeIcon icon={emptyStar} />}
+                        &nbsp;
+                        { numLikes }
+                        &nbsp;
+                        &nbsp;
+                    </div>
+                    <div id="postCommentsSummary">
+                        {numComments !== 0 ? <FontAwesomeIcon icon={faComment} /> : <FontAwesomeIcon icon={emptyComment} />}
+                        &nbsp;
+                        {numComments}
+
+                    </div>
+                </div>
             </div>
+            
         </div>
     )
 
